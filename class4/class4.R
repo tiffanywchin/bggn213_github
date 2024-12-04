146401d959bbaa58447ffedb18b13672e2335eb7
Tiffany Chin <t3chin@ucsd.edu>
2024-12-04 15:23:56 -0800

    class 4-6 work

---
+++ class4/class4.R
@@ -0,0 +1,10 @@
+#class4 script
+x <- 1:50
+plot(x)
+
+plot(x, sin(x), typ='l', col='green')
+plot(x=x, y=sin(x), typ='l', col='green')
+
+log(10) #this will do default base 
+log(10, base=10)
+log(10, 10)
